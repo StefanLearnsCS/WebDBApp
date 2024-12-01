7fc55c62514210b1299a0cf6516b63ebfc872cc0
Stefan <stefancool00@gmail.com>
2024-11-30 18:05:15 -0800

    Initialized ASP.NET, created models

---
+++ Auctions/Auctions/Models/Listing.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsSold { get; set; }
+
+        [Required]
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser User { get; set; }
+
+        public List<Bid>? Bids { get; set; }
+        public List<Comment>? Comments { get; set; }
+    }
+}
