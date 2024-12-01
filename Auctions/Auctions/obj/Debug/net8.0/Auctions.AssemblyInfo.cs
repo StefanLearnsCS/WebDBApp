2a500f0daae753ab3fcf3a9023569bf506e7efed
Stefan <stefancool00@gmail.com>
2024-12-01 13:06:14 -0800

    CSS and Pagination

--- Auctions/Auctions/obj/Debug/net8.0/Auctions.AssemblyInfo.cs
+++ Auctions/Auctions/obj/Debug/net8.0/Auctions.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auctions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb8bb2c73578c37f4aa410ab0fe6f89addfd8ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e68047ad13a57a2a29164fbf18f66750fc80b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auctions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auctions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
